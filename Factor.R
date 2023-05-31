7f80504379b39b6359a895aff9e3a79e12378168
TONGANNGUYEN <128988861+TONGANNGUYEN@users.noreply.github.com>
2023-05-31 20:42:51 +0700

    Add files via upload

---
+++ Factor.R
@@ -0,0 +1,5 @@
+# Create a factor
+music_genre <- factor(c("Jazz", "Rock", "Classic", "Classic", "Pop", "Jazz", "Rock", "Jazz"))
+
+# Print the factor
+music_genre
---
+++ InsertionSort.R
@@ -0,0 +1,38 @@
+# insertion sort function to sort array
+insertion_sort <- function(x)
+{
+  # calculate the length of array
+  n <- length(x)
+  # outer loop
+  for (i in 2 : (n))
+  {
+    # store first element as key
+    key = x[i]
+    j   = i - 1
+    # compare key with elements for
+    # its correct position
+    while (j > 0 && x[j] > key)
+    {
+      x[j + 1] = x[j]
+      j = j - 1
+    }
+    # Place key at its correct position
+    x[j + 1] = key
+  }
+  # return sorted array
+  x
+}
+
+# take sample array
+arr <- sample(1 : 100, 10)
+
+# call insertion sort function
+sorted_arr <- insertion_sort(arr)
+
+print("Chuong trinh truoc khi sap xep: ")
+arr
+
+# print sorted array
+print("Ket qua sau khi sap xep: ")
+sorted_arr
+
